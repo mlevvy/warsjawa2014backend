1ea234af61a4e5c79a1f902b6714a58b1354692f
Michał Lewandowski <mlw@touk.pl>
2014-08-16 13:51:42 +0200

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ MAILGUN_API_KEY=$3
 docker stop ${CONTAINER_NAME}
 docker rm ${CONTAINER_NAME}
 docker build -t ${CONTAINER_NAME} .
-docker run --name ${CONTAINER_NAME} --link ${DB_NAME}:db -d MAILGUN_API_KEY=${MAILGUN_API_KEY} ${CONTAINER_NAME}
+docker run --name ${CONTAINER_NAME} --link ${DB_NAME}:db -d -e MAILGUN_API_KEY=${MAILGUN_API_KEY} ${CONTAINER_NAME}
